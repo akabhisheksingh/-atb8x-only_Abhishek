b813c5494ae046578a3d2c2d2b3e5341f44682c3
Abhishek Singh <akabhisheksingh418@gmail.com>
2024-09-25 15:00:00 +0530

    First task

---
+++ src/multiThreadingTutorial/FirstTaskOn13thSept.java
@@ -0,0 +1,31 @@
+package multiThreadingTutorial;
+
+public class FirstTaskOn13thSept {
+	public static void main(String[] args) {
+		  int num= 10;
+		    System.out.printf("%d * %d = %d",num,1,num*1);
+		    System.out.println();
+		    System.out.printf("%d * %d = %d",num,2,num*1);
+		    System.out.println();
+		    System.out.printf("%d * %d = %d",num,3,num*1);
+		    System.out.println();
+		    System.out.printf("%d * %d = %d",num,4,num*1);
+		    System.out.println();
+		    System.out.printf("%d * %d = %d",num,5,num*1);
+		    System.out.println();
+		    System.out.printf("%d * %d = %d",num,6,num*1);
+		    System.out.println();
+		    System.out.printf("%d * %d = %d",num,7,num*1);
+		    System.out.println();
+		    System.out.printf("%d * %d = %d",num,8,num*1);
+		    System.out.println();
+		    System.out.printf("%d * %d = %d",num,9,num*1);
+		    System.out.println();
+		    System.out.printf("%d * %d = %d",num,10,num*1);
+		    System.out.println();
+	}
+
+
+
+
+}
