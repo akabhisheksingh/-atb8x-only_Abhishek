d0819394bbcd016064281bf729287d5da596da31
hp <text1@text.com>
2024-09-25 15:18:10 +0530

    Second program of task which was given on 13th sept

---
+++ src/LearningJava/CalculatorTask.java
@@ -0,0 +1,21 @@
+package LearningJava;
+
+public class CalculatorTask {
+	public static void main(String[] args) {
+		System.out.println(":::::::::::::::::::::::Caluculator::::::::::::::::");
+		double a = 34; 
+		double b = 10; 
+		
+		System.out.println("Addition");
+		System.out.println(a+b);
+		System.out.println("subtraction");
+		System.out.println(a-b);
+		System.out.println("Multiplication");
+		System.out.println(a*b);
+		System.out.println("division");
+		System.out.println(a/b);
+		System.out.println("Modulo Operation");
+		System.out.println(a%b);
+	}
+
+}
