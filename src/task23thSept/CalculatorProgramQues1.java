861b4516fc9dc8719c76a86120d8c018481808c5
Abhishek Kumar Singh <akabhisheksingh418@gmail.com>
2024-09-30 20:45:57 +0530

    Calculator program by switch case

---
+++ src/task23thSept/CalculatorProgramQues1.java
@@ -0,0 +1,47 @@
+package task23thSept;
+
+import java.util.Scanner;
+
+public class CalculatorProgramQues1 {
+	/*Create a simple calculator that performs addition, subtraction, multiplication, and division, modus based 
+	 * on user input using switch statements.
+	 * Inputs :   num 1, num 2, +
+	 * Output :  num1+num2 → print information.
+	 */
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the first number: ");
+		double num1 = sc.nextDouble();
+
+		System.out.println("Enter the second number: ");
+		double num2 = sc.nextDouble();
+
+		System.out.println("What kind of opeation: You want to do in Calculator:");
+		String operation = sc.next();
+		double value = 0.0;
+
+		switch (operation) {
+		case "+":
+			value = num1 + num2;
+			break;
+		case "-":
+			value = num1 - num2;
+			break;
+		case "*":
+			value = num1 * num2;
+			break;
+		case "/":
+			value = num1 / num2;
+			break;
+		case "%":
+			value = num1 % num2;
+			break;
+		default:
+			System.out.println("You can do only these operation +,-,*,/ or %.");
+		}
+		System.out.println("Result = " + value);
+
+	}
+
+}
